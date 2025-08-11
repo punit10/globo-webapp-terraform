16a621197e0caaf0e61611e2856f4c732f3f6e53
Punit Sharma <punitcs0067@gmail.com>
2025-08-11 12:12:46 +0530

    Switch to using secrets manager

--- resources.tf
+++ resources.tf
@@ -24,12 +24,19 @@ locals {
 ##################################################################################
 # RESOURCES
 ##################################################################################
+resource "aws_iam_instance_profile" "main" {
+  name = "${local.name_prefix}-webapp"
+  role = var.ec2_role_name
+
+  tags = local.common_tags
+}
 
 resource "aws_instance" "main" {
-  count         = length(data.tfe_outputs.networking.nonsensitive_values.public_subnets)
-  ami           = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
-  instance_type = var.instance_type
-  subnet_id     = data.tfe_outputs.networking.nonsensitive_values.public_subnets[count.index]
+  count                = length(data.tfe_outputs.networking.nonsensitive_values.public_subnets)
+  ami                  = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
+  iam_instance_profile = aws_iam_instance_profile.main
+  instance_type        = var.instance_type
+  subnet_id            = data.tfe_outputs.networking.nonsensitive_values.public_subnets[count.index]
   vpc_security_group_ids = [
     aws_security_group.webapp_http_inbound_sg.id,
     aws_security_group.webapp_ssh_inbound_sg.id,
@@ -44,6 +51,9 @@ resource "aws_instance" "main" {
   user_data_replace_on_change = true
   user_data = templatefile("./templates/userdata.sh", {
     playbook_repository = var.playbook_repository
+    secret_id           = var.api_key_secret_id
+    host_list_ssm_name  = local.host_list_ssm_name
+    site_name_ssm_name  = local.host_list_ssm_name
   })
 
   # # Provisioner Stuff
@@ -98,6 +108,7 @@ resource "null_resource" "webapp" {
 }
 */
 
+/* 
 resource "terraform_data" "webapp" {
   triggers_replace = [
     length(aws_instance.main.*.id),
@@ -122,6 +133,8 @@ resource "terraform_data" "webapp" {
   }
 
 }
+*/
+
 /* 
 resource "aws_lb" "main" {
   name               = "${local.name_prefix}-webapp"
