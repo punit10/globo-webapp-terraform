89094e68b02ac34cbf791a4f060dd151414a1ac0
Punit Sharma <punitcs0067@gmail.com>
2025-08-11 13:17:20 +0530

    updating output after app is working!

--- outputs.tf
+++ outputs.tf
@@ -11,7 +11,11 @@ output "webapp_instance1_public_ip" {
 }
 
 output "webapp_instance0_public_dns" {
-  value = aws_instance.main[1].public_dns
+  value = "http://${aws_instance.main[0].public_dns}"
+}
+
+output "webapp_instance1_public_dns" {
+  value = "http://${aws_instance.main[1].public_dns}"
 }
 
 output "private_key_pem" {
